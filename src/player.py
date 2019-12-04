01d787cbdeefcb248b27ef01d925a8961cf61870
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-12-04 16:45:57 -0500

    inventory maybe

--- src/player.py
+++ src/player.py
@@ -1,9 +1,18 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
 
     def __str__(self):
         return f"{self.name} is in {self.current_room}"
+
+    def item_grab(self, item_name):
+        for item in self.inventory:
+            if item_name == item_name:
+                return item
+            else:
+                return None
+
